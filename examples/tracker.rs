712a482b6dbdc3b8192e8a3f5dd92cbb78f3483c
Aaron Erhardt <aaron.erhardt@t-online.de>
2021-07-06 08:41:21 +0200

    Add tracker example
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

---
+++ examples/tracker.rs
@@ -0,0 +1,150 @@
+use glib::{object::Cast, Sender};
+use gtk::prelude::{ButtonExt, GtkWindowExt};
+use gtk::StringList;
+use relm4::*;
+use struct_tracker::Tracker;
+
+struct AppWidgets {
+    main: gtk::ApplicationWindow,
+    cntr_box: gtk::CenterBox,
+    toggle: gtk::ToggleButton,
+}
+
+enum AppMsg {
+    Toggle,
+    ShowLabel,
+    ShowButton,
+    ShowSlider,
+}
+
+enum WidgetSelection {
+    Label,
+    Button,
+    Slider,
+}
+
+// The proc macro allows to easily track changes to different
+// members of the model
+#[struct_tracker::tracker]
+struct AppModel {
+    counter: u8,
+    active_widget: WidgetSelection,
+}
+
+impl Widget<AppMsg, AppModel> for AppWidgets {
+    type Root = gtk::ApplicationWindow;
+
+    fn init_view(sender: Sender<AppMsg>, model: &AppModel) -> Self {
+        let main = gtk::ApplicationWindowBuilder::new()
+            .default_width(300)
+            .default_height(200)
+            .build();
+        let cntr_box = gtk::CenterBox::builder()
+            .orientation(gtk::Orientation::Vertical)
+            .margin_end(5)
+            .margin_top(5)
+            .margin_start(5)
+            .margin_bottom(5)
+            .build();
+
+        let options = vec!["label", "button", "slider"];
+        let list = StringList::new(&options);
+
+        let drop_down = gtk::DropDown::builder().model(&list).build();
+        let widget = gtk::Label::new(Some("Label"));
+        let toggle = gtk::ToggleButton::with_label(&format!("Toggled: {}", model.counter));
+
+        cntr_box.set_start_widget(Some(&drop_down));
+        cntr_box.set_center_widget(Some(&widget));
+        cntr_box.set_end_widget(Some(&toggle));
+        main.set_child(Some(&cntr_box));
+
+        let cloned_sender = sender.clone();
+        drop_down.connect_selected_item_notify(move |drop_down| {
+            let msg = match drop_down.selected() {
+                0 => AppMsg::ShowLabel,
+                1 => AppMsg::ShowButton,
+                2 => AppMsg::ShowSlider,
+                _ => return,
+            };
+            cloned_sender.send(msg).unwrap();
+        });
+
+        toggle.connect_clicked(move |_button| {
+            sender.send(AppMsg::Toggle).unwrap();
+        });
+
+        AppWidgets {
+            main,
+            cntr_box,
+            toggle,
+        }
+    }
+
+    fn root_widget(&self) -> gtk::ApplicationWindow {
+        self.main.clone()
+    }
+}
+
+impl AppUpdate<AppMsg> for AppModel {
+    type Widgets = AppWidgets;
+
+    fn init_model() -> Self {
+        AppModel {
+            counter: 0,
+            active_widget: WidgetSelection::Label,
+            tracker: 0,
+        }
+    }
+
+    fn update(&mut self, msg: AppMsg, _widgets: &Self::Widgets) {
+        // reset tracker value of the model
+        self.reset();
+        // set_#member_name() will set a bit in the tracker variable of the model
+        match msg {
+            AppMsg::Toggle => self.set_counter(self.get_counter().saturating_add(1)),
+            AppMsg::ShowLabel => {
+                self.set_active_widget(WidgetSelection::Label);
+            }
+            AppMsg::ShowButton => {
+                self.set_active_widget(WidgetSelection::Button);
+            }
+            AppMsg::ShowSlider => {
+                self.set_active_widget(WidgetSelection::Slider);
+            }
+        }
+        println!("counter: {}", self.counter);
+    }
+
+    fn view(&self, widgets: &mut Self::Widgets) {
+        // Only update the widget if model.active_widget was actually changed.
+        // This can be simply done by checking bits in the tracker variable of the member struct.
+        if self.changed(AppModel::active_widget()) {
+            let widget: gtk::Widget = match self.get_active_widget() {
+                WidgetSelection::Label => gtk::Label::new(Some("Label")).upcast::<gtk::Widget>(),
+                WidgetSelection::Button => {
+                    gtk::Button::with_label("Button").upcast::<gtk::Widget>()
+                }
+                WidgetSelection::Slider => {
+                    gtk::Scale::with_range(gtk::Orientation::Horizontal, 0.0, 100.0, 1.0)
+                        .upcast::<gtk::Widget>()
+                }
+            };
+            widgets.cntr_box.set_center_widget(Some(&widget));
+        }
+        // Only update toggle button if model.counter was actually changed
+        if self.changed(AppModel::counter()) {
+            widgets
+                .toggle
+                .set_label(&format!("Toggled: {}", self.counter));
+        }
+    }
+}
+
+fn main() {
+    gtk::init().unwrap();
+    let relm: RelmApp<AppWidgets, AppModel, AppMsg> = RelmApp::create();
+    relm.run();
+}
+
+
