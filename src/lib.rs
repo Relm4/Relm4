6d28855c4327399f1f967949ea3d5aecf03ee988
Aaron Erhardt <aaron.erhardt@t-online.de>
2021-08-02 17:23:06 +0200

    Add set_global_css functions
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,24 @@ pub use gtk::glib::Sender;
 #[cfg(feature = "tokio-rt")]
 pub use async_trait::async_trait;
 
+pub fn set_global_css(style_data: &[u8]) {
+    let display = gtk::gdk::Display::default().unwrap();
+    let provider = gtk::CssProvider::new();
+    provider.load_from_data(style_data);
+    gtk::StyleContext::add_provider_for_display(&display, &provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
+}
+
+pub fn set_global_css_from_file<P: AsRef<std::path::Path>>(path: P) {
+    match std::fs::read(path) {
+        Ok(bytes) => {
+            set_global_css(&bytes);
+        }
+        Err(err) => {
+            log::error!("Couln't load global CSS from file: {}", err);
+        }
+    }
+}
+
 pub fn spawn_future<F: futures_core::future::Future<Output = ()> + Send + 'static>(f: F) {
     gtk::glib::MainContext::ref_thread_default().spawn(f);
 }
