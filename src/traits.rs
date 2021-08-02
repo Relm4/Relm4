19492612ca798f75e77d6243f49f832bc56e6570
Aaron Erhardt <aaron.erhardt@t-online.de>
2021-08-02 17:23:28 +0200

    Add WidgetPlus trait
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- src/traits.rs
+++ src/traits.rs
@@ -1,4 +1,5 @@
 use gtk::glib::{self, Sender};
+use gtk::prelude::StyleContextExt;
 
 /// Widgets are part of an app or components. They represent the UI
 /// that usually consists out of GTK widgets. The root represents the
@@ -54,6 +55,32 @@ pub trait AsyncComponentUpdate<Components, Msg, ParentModel, ParentMsg> {
         sender: Sender<Msg>,
         parent_sender: Sender<ParentMsg>,
     );
+}
+
+pub trait WidgetPlus {
+    fn set_margin_all(&self, margin: i32);
+    fn add_class_name(&self, class: &str);
+    fn inline_css(&self, style_data: &[u8]);
+}
+
+impl<W: gtk::prelude::WidgetExt> WidgetPlus for W {
+    fn set_margin_all(&self, margin: i32) {
+        self.set_margin_start(margin);
+        self.set_margin_end(margin);
+        self.set_margin_top(margin);
+        self.set_margin_bottom(margin);
+    }
+
+    fn add_class_name(&self, class: &str) {
+        self.style_context().add_class(class);
+    }
+
+    fn inline_css(&self, style_data: &[u8]) {
+        let context = self.style_context();
+        let provider = gtk::CssProvider::new();
+        provider.load_from_data(&[b"*{", style_data, b"}"].concat());
+        context.add_provider(&provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION + 1);
+    }
 }
 
 impl<ParentModel, ParentMsg> RelmComponents<ParentModel, ParentMsg> for () {
