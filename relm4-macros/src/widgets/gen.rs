7770246ccab26d05c26f2e1a099254c1ff10086e
Aaron Erhardt <aaron.erhardt@t-online.de>
2021-08-03 22:14:14 +0200

    Improve macro structure
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

---
+++ relm4-macros/src/widgets/gen.rs
@@ -0,0 +1,222 @@
+use proc_macro2::TokenStream as TokenStream2;
+use quote::{quote, quote_spanned, ToTokens};
+use syn::{spanned::Spanned, *};
+
+use super::{PropertyType, Tracker, Widget, WidgetFunc};
+
+impl Tracker {
+    fn bool_eqation_tokens(&self) -> TokenStream2 {
+        let mut tokens = TokenStream2::new();
+
+        let mut iter = self.items.iter();
+        let first = iter.next().expect("No items to be tracked");
+        tokens.extend(quote_spanned! { first.span() => #first });
+
+        for item in iter {
+            tokens.extend(quote_spanned! { item.span() => || #item });
+        }
+
+        tokens
+    }
+}
+
+impl PropertyType {
+    fn init_assign_tokens(&self) -> Option<TokenStream2> {
+        match self {
+            PropertyType::Expr(expr) => Some(quote_spanned! { expr.span() => #expr }),
+            PropertyType::Value(lit) => Some(quote_spanned! { lit.span() => #lit }),
+            PropertyType::Widget(widget) => Some(widget.property_assignment()),
+            PropertyType::Watch(tokens) => Some(quote_spanned! { tokens.span() => #tokens }),
+            PropertyType::Track(Tracker { update_fn, .. }) => {
+                Some(quote_spanned! { update_fn.span() => #update_fn })
+            }
+            _ => None,
+        }
+    }
+
+    fn view_assign_tokens(&self) -> Option<TokenStream2> {
+        match self {
+            PropertyType::Watch(token_stream) => {
+                Some(quote_spanned! { token_stream.span() => #token_stream })
+            }
+            _ => None,
+        }
+    }
+
+    fn connect_assign_tokens(&self) -> Option<TokenStream2> {
+        if let PropertyType::Connect(closure) = self {
+            Some(quote_spanned! { closure.span() => #closure})
+        } else {
+            None
+        }
+    }
+
+    fn track_tokens(&self) -> Option<(TokenStream2, TokenStream2)> {
+        if let PropertyType::Track(tracker) = self {
+            let update_fn = &tracker.update_fn;
+            let update_stream = quote_spanned! { update_fn.span() => #update_fn };
+            let bool_stream = tracker.bool_eqation_tokens();
+            Some((bool_stream, update_stream))
+        } else {
+            None
+        }
+    }
+}
+
+impl WidgetFunc {
+    pub fn type_token_stream(&self) -> TokenStream2 {
+        let mut tokens = TokenStream2::new();
+        let segments = if let Some(ty) = &self.ty {
+            &ty[..]
+        } else if self.args.is_some() {
+            let len = self.path_segments.len();
+            if len < 1 {
+                return Error::new(self.span().unwrap().into(), "TODO").into_compile_error();
+            }
+            let last_index = len - 1;
+            &self.path_segments[0..last_index]
+        } else {
+            &self.path_segments[..]
+        };
+
+        let mut seg_iter = segments.iter();
+        tokens.extend(
+            seg_iter
+                .next()
+                .expect("No path segments in WidgetFunc")
+                .to_token_stream(),
+        );
+
+        for segment in seg_iter {
+            tokens.extend(quote! {::});
+            tokens.extend(segment.to_token_stream());
+        }
+
+        tokens
+    }
+
+    pub fn func_token_stream(&self) -> TokenStream2 {
+        let mut tokens = TokenStream2::new();
+
+        let mut seg_iter = self.path_segments.iter();
+        tokens.extend(
+            seg_iter
+                .next()
+                .expect("No path segments in WidgetFunc")
+                .to_token_stream(),
+        );
+
+        for segment in seg_iter {
+            tokens.extend(quote! {::});
+            tokens.extend(segment.to_token_stream());
+        }
+
+        if let Some(args) = &self.args {
+            tokens.extend(quote! {(#args)});
+            tokens
+        } else {
+            quote! {
+                <#tokens as relm4::default_widgets::DefaultWidget>::default_widget()
+            }
+        }
+    }
+}
+
+impl Widget {
+    pub fn return_stream(&self) -> TokenStream2 {
+        let w_span = self.func.span();
+        let w_name = &self.name;
+        quote_spanned! {
+            w_span => #w_name,
+        }
+    }
+
+    pub fn property_assignment(&self) -> TokenStream2 {
+        let w_span = self.func.span();
+        let w_name = &self.name;
+        let out_stream = if self.assign_as_ref {
+            quote_spanned! { w_span => & #w_name}
+        } else {
+            quote_spanned! { w_span => #w_name}
+        };
+        if let Some(wrapper) = &self.wrapper {
+            quote_spanned! {
+                wrapper.span() => #wrapper(#out_stream)
+            }
+        } else {
+            out_stream
+        }
+    }
+
+    pub fn view_stream(&self) -> TokenStream2 {
+        let w_name = &self.name;
+        let mut property_stream = TokenStream2::new();
+
+        for prop in &self.properties.properties {
+            let p_assign_opt = prop.ty.view_assign_tokens();
+            if let Some(p_assign) = p_assign_opt {
+                let p_name = &prop.name;
+                let p_span = p_name.span().unwrap().into();
+                property_stream.extend(quote_spanned! {
+                    p_span => self.#w_name.#p_name(#p_assign);
+                });
+            }
+        }
+
+        property_stream
+    }
+
+    pub fn property_stream(&self) -> TokenStream2 {
+        let w_name = &self.name;
+        let mut property_stream = TokenStream2::new();
+
+        for prop in &self.properties.properties {
+            let p_assign_opt = prop.ty.init_assign_tokens();
+            if let Some(p_assign) = p_assign_opt {
+                let p_name = &prop.name;
+                let p_span = p_name.span().unwrap().into();
+                property_stream.extend(quote_spanned! {
+                    p_span => #w_name.#p_name(#p_assign);
+                });
+            }
+        }
+
+        property_stream
+    }
+
+    pub fn connect_stream(&self) -> TokenStream2 {
+        let w_name = &self.name;
+        let mut stream = TokenStream2::new();
+
+        for prop in &self.properties.properties {
+            let p_assign_opt = prop.ty.connect_assign_tokens();
+            if let Some(p_assign) = p_assign_opt {
+                let p_name = &prop.name;
+                let p_span = p_name.span().unwrap().into();
+                stream.extend(quote_spanned! {
+                    p_span => #w_name.#p_name(#p_assign);
+                });
+            }
+        }
+
+        stream
+    }
+
+    pub fn track_stream(&self) -> TokenStream2 {
+        let w_name = &self.name;
+        let mut stream = TokenStream2::new();
+
+        for prop in &self.properties.properties {
+            let p_assign_opt = prop.ty.track_tokens();
+            if let Some((bool_stream, update_stream)) = p_assign_opt {
+                let p_name = &prop.name;
+                let p_span = p_name.span().unwrap().into();
+                stream.extend(quote_spanned! {
+                    p_span =>  if #bool_stream {
+                        self.#w_name.#p_name(#update_stream);
+                }});
+            }
+        }
+        stream
+    }
+}
---
+++ relm4-macros/src/widgets/mod.rs
@@ -0,0 +1,99 @@
+use proc_macro::Span;
+use proc_macro2::Span as Span2;
+use proc_macro2::TokenStream as TokenStream2;
+use syn::{punctuated::Punctuated, spanned::Spanned, *};
+
+mod gen;
+mod parse;
+
+#[derive(Debug)]
+pub(super) struct Tracker {
+    items: Vec<Expr>,
+    update_fn: Expr,
+}
+
+#[derive(Debug)]
+pub(super) enum PropertyType {
+    Expr(Expr),
+    Value(Lit),
+    Track(Tracker),
+    Connect(ExprClosure),
+    Watch(TokenStream2),
+    Widget(Widget),
+}
+
+#[derive(Debug)]
+pub(super) struct Property {
+    pub name: Ident,
+    pub ty: PropertyType,
+}
+
+#[derive(Debug)]
+pub(super) struct Properties {
+    pub properties: Vec<Property>,
+}
+
+#[derive(Debug)]
+pub(super) struct WidgetFunc {
+    pub path_segments: Vec<Ident>,
+    pub args: Option<Punctuated<Expr, token::Comma>>,
+    pub ty: Option<Vec<Ident>>,
+}
+
+impl Spanned for WidgetFunc {
+    fn span(&self) -> Span2 {
+        self.path_segments
+            .first()
+            .expect("No segments in WidgetFunc")
+            .span()
+    }
+}
+
+#[derive(Debug)]
+pub(super) struct Widget {
+    pub name: Ident,
+    pub func: WidgetFunc,
+    pub properties: Properties,
+    pub wrapper: Option<Ident>,
+    pub assign_as_ref: bool,
+}
+
+impl<'a> Widget {
+    pub fn get_widget_list(&'a self, widgets: &mut Vec<&'a Widget>) {
+        widgets.push(self);
+
+        for prop in &self.properties.properties {
+            let ty = &prop.ty;
+            if let PropertyType::Widget(widget) = ty {
+                widget.get_widget_list(widgets);
+            }
+        }
+    }
+}
+
+impl Widget {
+    pub fn new(span: Span, items: &[ImplItem]) -> Result<Self> {
+        let mut widgets = None;
+        for item in items {
+            if let ImplItem::Macro(mac) = item {
+                if mac
+                    .mac
+                    .path
+                    .segments
+                    .first()
+                    .expect("No path segments in macro path")
+                    .ident
+                    == "view"
+                {
+                    let tokens = mac.mac.tokens.clone();
+                    let new_widgets = syn::parse_macro_input::parse::<Widget>(tokens.into())?;
+                    widgets = Some(new_widgets);
+                } else {
+                    return Err(Error::new(item.span().unwrap().into(), "Unexpected macro"));
+                }
+            }
+        }
+
+        widgets.ok_or_else(|| Error::new(span.into(), "No view macro in impl block"))
+    }
+}
