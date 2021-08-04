96bd949b2413afc63bf99445f00fc1eadf955c4c
Aaron Erhardt <aaron.erhardt@t-online.de>
2021-08-04 22:58:48 +0200

    Add example for simple app
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

---
+++ relm4-examples/examples/simple.rs
@@ -0,0 +1,68 @@
+use relm4::{impl_model, RelmWidgets, AppUpdate, Sender, send, RelmApp, WidgetPlus};
+use gtk::prelude::{ButtonExt, BoxExt, GtkWindowExt, OrientableExt};
+
+#[derive(Default)]
+struct AppModel {
+    counter: u8,
+}
+
+enum AppMsg {
+    Increment,
+    Decrement,
+}
+
+impl_model!(AppModel, AppMsg);
+
+impl AppUpdate for AppModel {
+    fn update(&mut self, msg: AppMsg, _components: &(), _sender: Sender<AppMsg>) {
+        match msg {
+            AppMsg::Increment => {
+                self.counter = self.counter.wrapping_add(1);
+            }
+            AppMsg::Decrement => {
+                self.counter = self.counter.wrapping_sub(1);
+            }
+        }
+    }
+}
+
+#[relm4_macros::widget]
+impl RelmWidgets for AppWidgets {
+    type Model = AppModel;
+
+    view! {
+        gtk::ApplicationWindow {
+            set_title: Some("Simple app"),
+            set_default_width: 300,
+            set_default_height: 100,
+            set_child = Some(&gtk::Box) {
+                set_orientation: gtk::Orientation::Vertical,
+                set_margin_all: 5,
+                set_spacing: 5,
+
+                append = &gtk::Button {
+                    set_label: "Increment",
+                    connect_clicked => move |_| {
+                        send!(sender, AppMsg::Increment);
+                    },
+                },
+                append = &gtk::Button {
+                    set_label: "Decrement",
+                    connect_clicked => move |_| {
+                        send!(sender, AppMsg::Decrement);
+                    },
+                },
+                append = &gtk::Label {
+                    set_margin_all: 5,
+                    set_label: watch! { &format!("Counter: {}", model.counter) },
+                }
+            },
+        }
+    }
+}
+
+fn main() {
+    let model = AppModel::default();
+    let app: RelmApp<AppWidgets> = RelmApp::new(model);
+    app.run();
+}
