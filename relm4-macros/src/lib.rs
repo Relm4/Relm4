de6844d98c4d5aa44472d75ccdbcff8dbb9afc48
Aaron Erhardt <aaron.erhardt@t-online.de>
2021-08-10 23:27:02 +0200

    Bump versions to 0.1.0-beta.2
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- relm4-macros/src/lib.rs
+++ relm4-macros/src/lib.rs
@@ -16,10 +16,83 @@ use item_impl::ItemImpl;
 use macros::Macros;
 use types::ModelTypes;
 
+/// Macro that implemements [relm4::Widgets](https://aaronerhardt.github.io/docs/relm4/relm4/trait.Widgets.html) and generates the corresponding struct.
+///
+/// # Attributes
+///
+/// Use `#[widget(pub)]` to create a public struct.
+///
+/// # Example
+///
+/// ```
+/// use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt, OrientableExt};
+/// use relm4::{send, AppUpdate, Model, RelmApp, Sender, WidgetPlus, Widgets};
+///
+/// #[derive(Default)]
+/// struct AppModel {
+///     counter: u8,
+/// }
+///
+/// enum AppMsg {
+///     Increment,
+///     Decrement,
+/// }
+///
+/// impl Model for AppModel {
+///     type Msg = AppMsg;
+///     type Widgets = AppWidgets;
+///     type Components = ();
+/// }
+///
+/// impl AppUpdate for AppModel {
+///     fn update(&mut self, msg: AppMsg, _components: &(), _sender: Sender<AppMsg>) -> bool {
+///         match msg {
+///             AppMsg::Increment => {
+///                 self.counter = self.counter.wrapping_add(1);
+///             }
+///             AppMsg::Decrement => {
+///                 self.counter = self.counter.wrapping_sub(1);
+///             }
+///         }
+///         true
+///     }
+/// }
+///
+/// #[relm4_macros::widget]
+/// impl Widgets<AppModel, ()> for AppWidgets {
+///     view! {
+///         gtk::ApplicationWindow {
+///             set_title: Some("Simple app"),
+///             set_default_width: 300,
+///             set_default_height: 100,
+///             set_child = Some(&gtk::Box) {
+///                 set_orientation: gtk::Orientation::Vertical,
+///                 set_margin_all: 5,
+///                 set_spacing: 5,
+///
+///                 append = &gtk::Button {
+///                     set_label: "Increment",
+///                     connect_clicked(sender) => move |_| {
+///                         send!(sender, AppMsg::Increment);
+///                     },
+///                 },
+///                 append = &gtk::Button {
+///                     set_label: "Decrement",
+///                     connect_clicked(sender) => move |_| {
+///                         send!(sender, AppMsg::Decrement);
+///                     },
+///                 },
+///                 append = &gtk::Label {
+///                     set_margin_all: 5,
+///                     set_label: watch! { &format!("Counter: {}", model.counter) },
+///                 }
+///             },
+///         }
+///     }
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn widget(attributes: TokenStream, input: TokenStream) -> TokenStream {
-    let start = std::time::Instant::now();
-
     let attrs = parse_macro_input!(attributes as Attrs);
     let data = parse_macro_input!(input as ItemImpl);
 
@@ -131,10 +204,6 @@ pub fn widget(attributes: TokenStream, input: TokenStream) -> TokenStream {
             }
         }
     };
-
-    let end = std::time::Instant::now();
-    let duration = end - start;
-    eprintln!("INFO: widget macro took {} ms", duration.as_millis());
 
     out.into()
 }
