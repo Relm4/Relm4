17225cea5f108d146fbc021994e971f45f33ad41
Aaron Erhardt <aaron.erhardt@t-online.de>
2021-08-13 10:58:20 +0200

    Use MPL as license and MIT for examples
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- src/factory/mod.rs
+++ src/factory/mod.rs
@@ -1,3 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 //! Defines traits and data types used to efficiently generating widgets from collections.
 
 use gtk::glib::Sender;
