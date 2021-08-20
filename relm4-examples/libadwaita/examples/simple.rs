a1ac6a6250ca86b504e88129937b1d25adb8b380
Aaron Erhardt <aaron.erhardt@t-online.de>
2021-08-20 14:26:24 +0200

    Initial support for libadwaita
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

---
+++ relm4-examples/libadwaita/examples/simple.rs
@@ -0,0 +1,70 @@
+use gtk::prelude::{BoxExt, ButtonExt, OrientableExt};
+use adw::prelude::{WindowExt};
+use relm4::{send, AppUpdate, Model, RelmApp, Sender, WidgetPlus, Widgets};
+
+#[derive(Default)]
+struct AppModel {
+    counter: u8,
+}
+
+enum AppMsg {
+    Increment,
+    Decrement,
+}
+
+impl Model for AppModel {
+    type Msg = AppMsg;
+    type Widgets = AppWidgets;
+    type Components = ();
+}
+
+impl AppUpdate for AppModel {
+    fn update(&mut self, msg: AppMsg, _components: &(), _sender: Sender<AppMsg>) -> bool {
+        match msg {
+            AppMsg::Increment => {
+                self.counter = self.counter.wrapping_add(1);
+            }
+            AppMsg::Decrement => {
+                self.counter = self.counter.wrapping_sub(1);
+            }
+        }
+        true
+    }
+}
+
+#[relm4_macros::widget]
+impl Widgets<AppModel, ()> for AppWidgets {
+    view! {
+        adw::ApplicationWindow {
+            //set_title: Some("Simple app"),
+            set_child = Some(&gtk::Box) {
+                set_orientation: gtk::Orientation::Vertical,
+                set_margin_all: 5,
+                set_spacing: 5,
+
+                append = &gtk::Button {
+                    set_label: "Increment",
+                    connect_clicked(sender) => move |_| {
+                        send!(sender, AppMsg::Increment);
+                    },
+                },
+                append = &gtk::Button {
+                    set_label: "Decrement",
+                    connect_clicked(sender) => move |_| {
+                        send!(sender, AppMsg::Decrement);
+                    },
+                },
+                append = &gtk::Label {
+                    set_margin_all: 5,
+                    set_label: watch! { &format!("Counter: {}", model.counter) },
+                }
+            },
+        }
+    }
+}
+
+fn main() {
+    let model = AppModel::default();
+    let app = RelmApp::new(model);
+    app.run();
+}
