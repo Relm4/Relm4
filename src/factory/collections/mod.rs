5ad6793d72e03368f17110186f4b4a6b472bcc8b
Aaron Erhardt <aaron.erhardt@t-online.de>
2021-08-28 22:50:33 +0200

    Improve factory docs
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- src/factory/collections/mod.rs
+++ src/factory/collections/mod.rs
@@ -1,4 +1,10 @@
-//! Containers similar to [`std::collections`] that implement [`Factory`](super::Factory)
+//! Containers similar to [`std::collections`] that implement the [`Factory`](super::Factory) trait.
+//!
+//! # Which factory type to use
+//!
+//! Use [`FactoryVec`] if you only need to push and pop at the back.
+//! If you need more flexibility for example for pushing or removing items at
+//! arbitraty indices use [`FactoryVecDeque`].
 
 mod factory_vec;
 mod factory_vec_deque;
