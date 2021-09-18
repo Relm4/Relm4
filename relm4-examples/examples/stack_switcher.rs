bc8ea3f027a801126f767e93a3b04e20df4ca714
Aaron Erhardt <aaron.erhardt@t-online.de>
2021-09-18 17:20:13 +0200

    Add stack switcher example
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

---
+++ relm4-examples/examples/stack_switcher.rs
@@ -0,0 +1,155 @@
+use gtk::prelude::{BoxExt, GtkWindowExt, WidgetExt};
+use relm4::{
+    AppUpdate, ComponentUpdate, Components, Model, RelmApp, RelmComponent, Sender, Widgets,
+};
+
+enum HeaderMsg {
+    SetStack(gtk::Stack),
+}
+
+struct HeaderModel {
+    stack: Option<gtk::Stack>,
+}
+
+impl Model for HeaderModel {
+    type Msg = HeaderMsg;
+    type Widgets = HeaderWidgets;
+    type Components = ();
+}
+
+impl ComponentUpdate<AppModel> for HeaderModel {
+    fn init_model(_parent_model: &AppModel) -> Self {
+        HeaderModel { stack: None }
+    }
+
+    fn update(
+        &mut self,
+        msg: HeaderMsg,
+        _components: &(),
+        _sender: Sender<HeaderMsg>,
+        _parent_sender: Sender<AppMsg>,
+    ) {
+        let HeaderMsg::SetStack(stack) = msg;
+        self.stack = Some(stack);
+    }
+}
+
+#[relm4_macros::widget]
+impl Widgets<HeaderModel, AppModel> for HeaderWidgets {
+    view! {
+        gtk::HeaderBar {
+            set_title_widget = Some(&gtk::Box) {
+                add_css_class: "linked",
+                append: switcher = &gtk::StackSwitcher {
+                    set_stack: watch!(model.stack.as_ref()),
+                }
+            }
+        }
+    }
+}
+
+struct StackModel {}
+
+enum StackMsg {}
+
+impl Model for StackModel {
+    type Msg = StackMsg;
+    type Widgets = StackWidgets;
+    type Components = ();
+}
+
+impl ComponentUpdate<AppModel> for StackModel {
+    fn init_model(_parent_model: &AppModel) -> Self {
+        StackModel {}
+    }
+
+    fn update(
+        &mut self,
+        _msg: StackMsg,
+        _components: &(),
+        _sender: Sender<StackMsg>,
+        _parent_sender: Sender<AppMsg>,
+    ) {
+    }
+}
+
+#[relm4_macros::widget]
+impl Widgets<StackModel, AppModel> for StackWidgets {
+    view! {
+        gtk::Stack {
+            add_titled(Some("First"), "First") = &gtk::Label {
+                set_label: "First page"
+            },
+            add_titled(Some("Second"), "Second") = &gtk::Label {
+                set_label: "Second page"
+            }
+        }
+    }
+}
+
+struct AppComponents {
+    header: RelmComponent<HeaderModel, AppModel>,
+    stack: RelmComponent<StackModel, AppModel>,
+}
+
+impl Components<AppModel> for AppComponents {
+    fn init_components(
+        parent_model: &AppModel,
+        parent_widgets: &AppWidgets,
+        parent_sender: Sender<AppMsg>,
+    ) -> Self {
+        AppComponents {
+            header: RelmComponent::new(parent_model, parent_widgets, parent_sender.clone()),
+            stack: RelmComponent::new(parent_model, parent_widgets, parent_sender),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum AppMode {}
+
+enum AppMsg {}
+
+struct AppModel {}
+
+impl Model for AppModel {
+    type Msg = AppMsg;
+    type Widgets = AppWidgets;
+    type Components = AppComponents;
+}
+
+#[relm4_macros::widget]
+impl Widgets<AppModel, ()> for AppWidgets {
+    view! {
+        main_window = gtk::ApplicationWindow {
+            set_default_width: 500,
+            set_default_height: 250,
+            set_titlebar: component!(Some(components.header.root_widget())),
+            set_child: component!(Some(components.stack.root_widget())),
+        }
+    }
+
+    fn post_connect_components() {
+        components
+            .header
+            .send(HeaderMsg::SetStack(components.stack.root_widget().clone()))
+            .unwrap();
+    }
+}
+
+impl AppUpdate for AppModel {
+    fn update(
+        &mut self,
+        _msg: AppMsg,
+        _components: &AppComponents,
+        _sender: Sender<AppMsg>,
+    ) -> bool {
+        true
+    }
+}
+
+fn main() {
+    let model = AppModel {};
+    let relm = RelmApp::new(model);
+    relm.run();
+}
