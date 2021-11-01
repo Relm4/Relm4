6f602a43ae71e5c5b438836f7e8a16f389780f11
Peter Sonntaga <peter@sonniger-tag.eu>
2021-11-01 09:08:18 +0100

    add adw view-switcher example

---
+++ relm4-examples/libadwaita/examples/view-switcher.rs
@@ -0,0 +1,79 @@
+use adw::traits::ApplicationWindowExt;
+use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt, OrientableExt};
+use relm4::{send, AppUpdate, Model, RelmApp, Sender, WidgetPlus, Widgets};
+
+#[derive(Default)]
+struct AppModel {
+    counter: u8,
+}
+
+enum AppMsg {
+    Increment,
+    Decrement,
+}
+
+impl Model for AppModel {
+    type Msg = AppMsg;
+    type Widgets = AppWidgets;
+    type Components = ();
+}
+
+impl AppUpdate for AppModel {
+    fn update(&mut self, msg: AppMsg, _components: &(), _sender: Sender<AppMsg>) -> bool {
+        match msg {
+            AppMsg::Increment => {
+                self.counter = self.counter.wrapping_add(1);
+            }
+            AppMsg::Decrement => {
+                self.counter = self.counter.wrapping_sub(1);
+            }
+        }
+        true
+    }
+}
+
+fn application_window() -> adw::ApplicationWindow {
+    adw::ApplicationWindow::builder().build()
+}
+
+#[relm4_macros::widget]
+impl Widgets<AppModel, ()> for AppWidgets {
+    view! {
+        main_window = application_window() -> adw::ApplicationWindow {
+            set_default_width: 300,
+            set_default_height: 200,
+
+            set_content = Some(&gtk::Box) {
+                set_orientation: gtk::Orientation::Vertical,
+                set_margin_all: 5,
+                set_spacing: 5,
+
+                append = &adw::HeaderBar {
+                    set_title_widget = Some(&adw::ViewSwitcherTitle) {
+                        set_title: "A stack switcher App",
+                        set_stack: Some(&stack),
+                    }
+                },
+                
+                append: stack = &adw::ViewStack {
+                        add_titled: args!(
+                            &gtk::Label {
+                            set_label: "Start",
+                        }, Some("Title"), "title"),
+                        add: intro_label = &gtk::Label {
+                            set_label: "To get started click on \"Next page\".",
+                        },
+                        add: overview_label = &gtk::Label {
+                            set_label: "* Insert overview here *",
+                        },
+                }
+            },
+        }
+    }
+}
+
+fn main() {
+    let model = AppModel::default();
+    let app = RelmApp::new(model);
+    app.run();
+}
