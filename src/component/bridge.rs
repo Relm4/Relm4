9c53687036959ff6a41eacd5716d00a43081a088
Michael Aaron Murphy <mmstick@pm.me>
2022-01-27 19:52:05 +0100

    refactor: Drop component services by their GLib SourceID

--- src/component/bridge.rs
+++ src/component/bridge.rs
@@ -22,19 +22,10 @@ impl<C: Component> Bridge<C, C::Root> {
         let Bridge { root, .. } = self;
 
         // Used for all events to be processed by this component's internal service.
-        let (mut input_tx, input_rx) = mpsc::unbounded_channel::<C::Input>();
+        let (mut input_tx, mut input_rx) = mpsc::unbounded_channel::<C::Input>();
 
         // Used by this component to send events to be handled externally by the caller.
         let (mut output_tx, output_rx) = mpsc::unbounded_channel::<C::Output>();
-
-        // This channel enables forwards inputs to the internal service.
-        let (inner_tx, mut inner_rx) = mpsc::unbounded_channel::<InnerMessage<C::Input>>();
-
-        // Send a `Drop` message to the internal service.
-        root.as_ref()
-            .connect_destroy(gtk::glib::clone!(@strong inner_tx => move |_| {
-                let _ = inner_tx.send(InnerMessage::Drop);
-            }));
 
         // Constructs the initial model and view with the initial payload.
         let Fuselage {
@@ -42,35 +33,32 @@ impl<C: Component> Bridge<C, C::Root> {
             mut widgets,
         } = C::dock(payload, &root, &mut input_tx, &mut output_tx);
 
-        // Forward `Self::Input` events to the internal service.
-        let forwarder = crate::forward(input_rx, inner_tx, InnerMessage::Message);
-
         // The internal service which manages all input requests.
         let mut input_ = input_tx.clone();
-        let service = async move {
-            while let Some(event) = inner_rx.recv().await {
-                match event {
-                    InnerMessage::Message(event) => {
-                        // Performs the model update, checking if the update requested a command.
-                        // Runs that command asynchronously in the background using tokio.
-                        if let Some(command) = model.update(event, &mut input_, &mut output_tx) {
-                            let input = input_.clone();
-                            crate::spawn(async move {
-                                C::command(command, input).await;
-                            });
-                        }
 
-                        model.update_view(&mut widgets, &mut input_, &mut output_tx);
-                    }
-
-                    InnerMessage::Drop => break,
+        // Start the internal service.
+        let id = crate::spawn_local(async move {
+            while let Some(event) = input_rx.recv().await {
+                // Performs the model update, checking if the update requested a command.
+                // Runs that command asynchronously in the background using tokio.
+                if let Some(command) = model.update(event, &mut input_, &mut output_tx) {
+                    let input = input_.clone();
+                    crate::spawn(async move {
+                        C::command(command, input).await;
+                    });
                 }
-            }
-        };
 
-        // Start the internal service and its message forwarder.
-        crate::spawn_local(async move {
-            futures::future::join(forwarder, service).await;
+                model.update_view(&mut widgets, &mut input_, &mut output_tx);
+            }
+        });
+
+        // When the root widget is destroyed, the spawned service will be removed.
+        let id = std::cell::RefCell::new(Some(id));
+
+        root.as_ref().connect_destroy(move |_| {
+            if let Some(id) = id.borrow_mut().take() {
+                id.remove();
+            }
         });
 
         Fairing {
@@ -87,19 +75,10 @@ impl<C: StatefulComponent> Bridge<C, C::Root> {
         let Bridge { root, .. } = self;
 
         // Used for all events to be processed by this component's internal service.
-        let (mut input_tx, input_rx) = mpsc::unbounded_channel::<C::Input>();
+        let (mut input_tx, mut input_rx) = mpsc::unbounded_channel::<C::Input>();
 
         // Used by this component to send events to be handled externally by the caller.
         let (mut output_tx, output_rx) = mpsc::unbounded_channel::<C::Output>();
-
-        // This channel enables forwards inputs to the internal service.
-        let (inner_tx, mut inner_rx) = mpsc::unbounded_channel::<InnerMessage<C::Input>>();
-
-        // Send a `Drop` message to the internal service.
-        root.as_ref()
-            .connect_destroy(gtk::glib::clone!(@strong inner_tx => move |_| {
-                let _ = inner_tx.send(InnerMessage::Drop);
-            }));
 
         // Constructs the initial model and view with the initial payload.
         let Fuselage {
@@ -107,35 +86,30 @@ impl<C: StatefulComponent> Bridge<C, C::Root> {
             mut widgets,
         } = C::dock(payload, &root, &mut input_tx, &mut output_tx);
 
-        // Forward `Self::Input` events to the internal service.
-        let forwarder = crate::forward(input_rx, inner_tx, InnerMessage::Message);
-
         // The internal service which manages all input requests.
         let mut input_ = input_tx.clone();
-        let service = async move {
-            while let Some(event) = inner_rx.recv().await {
-                match event {
-                    InnerMessage::Message(event) => {
-                        // Performs the model update, checking if the update requested a command.
-                        // Runs that command asynchronously in the background using tokio.
-                        if let Some(command) =
-                            model.update(&mut widgets, event, &mut input_, &mut output_tx)
-                        {
-                            let input = input_.clone();
-                            crate::spawn(async move {
-                                C::command(command, input).await;
-                            });
-                        }
-                    }
-
-                    InnerMessage::Drop => break,
+        let id = crate::spawn_local(async move {
+            while let Some(event) = input_rx.recv().await {
+                // Performs the model update, checking if the update requested a command.
+                // Runs that command asynchronously in the background using tokio.
+                if let Some(command) =
+                    model.update(&mut widgets, event, &mut input_, &mut output_tx)
+                {
+                    let input = input_.clone();
+                    crate::spawn(async move {
+                        C::command(command, input).await;
+                    });
                 }
             }
-        };
+        });
 
-        // Start the internal service and its message forwarder.
-        crate::spawn_local(async move {
-            futures::future::join(forwarder, service).await;
+        // When the root widget is destroyed, the spawned service will be removed.
+        let id = std::cell::RefCell::new(Some(id));
+
+        root.as_ref().connect_destroy(move |_| {
+            if let Some(id) = id.borrow_mut().take() {
+                id.remove();
+            }
         });
 
         Fairing {
@@ -144,10 +118,4 @@ impl<C: StatefulComponent> Bridge<C, C::Root> {
             receiver: output_rx,
         }
     }
-}
-
-/// Used to drop the component's event loop when the managed widget is destroyed.
-enum InnerMessage<T> {
-    Drop,
-    Message(T),
 }
