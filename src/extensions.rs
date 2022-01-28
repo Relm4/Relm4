e42f55b3e27f9300880c5eb1b9025993bcbf4782
Michael Aaron Murphy <mmstick@pm.me>
2022-01-28 14:50:27 +0100

    feat: Add RelmWidgetExt trait with iter_children and for_each_child

--- src/extensions.rs
+++ src/extensions.rs
@@ -26,3 +26,38 @@ impl ApplicationBuilderExt for gtk::builders::ApplicationBuilder {
         app.run();
     }
 }
+
+/// Additional methods for `gtk::Widget`
+pub trait RelmWidgetExt {
+    /// Iterates across the child widgets of a widget
+    fn iter_children(&self) -> Box<dyn Iterator<Item = gtk::Widget>>;
+
+    /// Iterates children of a widget with a closure.
+    fn for_each_child<F: FnMut(gtk::Widget) + 'static>(&self, func: F);
+}
+
+impl<T: gtk::glib::IsA<gtk::Widget>> RelmWidgetExt for T {
+    fn for_each_child<F: FnMut(gtk::Widget) + 'static>(&self, mut func: F) {
+        let mut widget = self.first_child();
+
+        while let Some(child) = widget.take() {
+            widget = child.next_sibling();
+            func(child);
+        }
+    }
+
+    fn iter_children(&self) -> Box<dyn Iterator<Item = gtk::Widget>> {
+        let mut widget = self.first_child();
+
+        let iterator = std::iter::from_fn(move || {
+            if let Some(child) = widget.take() {
+                widget = child.next_sibling();
+                return Some(child);
+            }
+
+            None
+        });
+
+        Box::new(iterator)
+    }
+}
