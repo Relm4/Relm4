ad980044510206dc4f6d10a8efaf56e9be82bd70
Aaron Erhardt <aaron.erhardt@t-online.de>
2022-03-18 19:23:03 +0100

    Add more widgets
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- src/factory/widgets/gtk.rs
+++ src/factory/widgets/gtk.rs
@@ -1,4 +1,4 @@
-use gtk::prelude::{BoxExt, GridExt};
+use gtk::prelude::{BoxExt, Cast, GridExt, ListBoxRowExt, WidgetExt};
 
 use crate::factory::{positions, FactoryView, FactoryViewPlus};
 
@@ -37,8 +37,8 @@ impl FactoryViewPlus for gtk::Box {
         widget.as_ref().clone()
     }
 
-    fn returned_widget_to_child(root_child: &Self::ReturnedWidget) -> Self::Children {
-        root_child.clone()
+    fn returned_widget_to_child(returned_widget: &Self::ReturnedWidget) -> Self::Children {
+        returned_widget.clone()
     }
 
     fn factory_move_after(&self, widget: &Self::ReturnedWidget, other: &Self::ReturnedWidget) {
@@ -94,3 +94,54 @@ impl FactoryView for gtk::Stack {
         self.remove(&widget.child());
     }
 }
+
+impl FactoryView for gtk::ListBox {
+    type Children = gtk::Widget;
+    type ReturnedWidget = gtk::ListBoxRow;
+
+    type Position = ();
+
+    fn factory_append(
+        &self,
+        widget: impl AsRef<Self::Children>,
+        _position: &Self::Position,
+    ) -> Self::ReturnedWidget {
+        self.append(widget.as_ref());
+        widget.as_ref().parent().unwrap().downcast().unwrap()
+    }
+
+    fn factory_remove(&self, widget: &Self::ReturnedWidget) {
+        self.remove(&widget.child().unwrap());
+    }
+}
+
+impl FactoryViewPlus for gtk::ListBox {
+    fn factory_prepend(&self, widget: impl AsRef<Self::Children>) -> Self::ReturnedWidget {
+        self.prepend(widget.as_ref());
+        widget.as_ref().parent().unwrap().downcast().unwrap()
+    }
+
+    fn factory_insert_after(
+        &self,
+        widget: impl AsRef<Self::Children>,
+        other: &Self::ReturnedWidget,
+    ) -> Self::ReturnedWidget {
+        self.insert(widget.as_ref(), other.index());
+        widget.as_ref().parent().unwrap().downcast().unwrap()
+    }
+
+    fn factory_move_after(&self, widget: &Self::ReturnedWidget, other: &Self::ReturnedWidget) {
+        self.remove(widget);
+        self.insert(widget, other.index());
+    }
+
+    fn factory_move_start(&self, widget: &Self::ReturnedWidget) {
+        self.remove(widget);
+        self.prepend(widget);
+    }
+
+    fn returned_widget_to_child(returned_widget: &Self::ReturnedWidget) -> Self::Children {
+        returned_widget.child().unwrap()
+    }
+}
+
