074a5d01dcbc5c5840fb135e47b28d3e251f6395
Aaron Erhardt <aaron.erhardt@t-online.de>
2022-03-20 09:17:15 +0100

    Fix TabView factory implementation
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- relm4/src/factory/widgets/libadwaita.rs
+++ relm4/src/factory/widgets/libadwaita.rs
@@ -1,46 +1,51 @@
+use crate::factory::FactoryView;
+
 impl FactoryView for adw::TabView {
     type Children = gtk::Widget;
-    type ChildRoot = adw::TabPage;
+    type ReturnedWidget = adw::TabPage;
 
     type Position = ();
+
+    fn factory_remove(&self, widget: &Self::ReturnedWidget) {
+        self.close_page_finish(widget, true);
+    }
 
     fn factory_append(
         &self,
         widget: impl AsRef<Self::Children>,
         _position: &Self::Position,
-    ) -> Self::ChildRoot {
+    ) -> Self::ReturnedWidget {
         self.append(widget.as_ref())
     }
 
-    fn factory_remove(&self, widget: &Self::ChildRoot) {
-        self.close_page_finish(widget, true);
-    }
-}
-
-impl FactoryViewPlus for adw::TabView {
-    fn factory_prepend(&self, widget: impl AsRef<Self::Children>) -> Self::ChildRoot {
+    fn factory_prepend(
+        &self,
+        widget: impl AsRef<Self::Children>,
+        _position: &(),
+    ) -> Self::ReturnedWidget {
         self.prepend(widget.as_ref())
     }
 
     fn factory_insert_after(
         &self,
         widget: impl AsRef<Self::Children>,
-        other: &Self::ChildRoot,
-    ) -> Self::ChildRoot {
+        _position: &(),
+        other: &Self::ReturnedWidget,
+    ) -> Self::ReturnedWidget {
         let new_position = self.page_position(other) + 1;
         self.insert(widget.as_ref(), new_position)
     }
 
-    fn child_root_to_child(root_child: &Self::ChildRoot) -> Self::Children {
+    fn returned_widget_to_child(root_child: &Self::ReturnedWidget) -> Self::Children {
         root_child.child()
     }
 
-    fn factory_move_after(&self, widget: &Self::ChildRoot, other: &Self::ChildRoot) {
+    fn factory_move_after(&self, widget: &Self::ReturnedWidget, other: &Self::ReturnedWidget) {
         let new_position = self.page_position(other) + 1;
         self.reorder_page(widget, new_position);
     }
 
-    fn factory_move_start(&self, widget: &Self::ChildRoot) {
+    fn factory_move_start(&self, widget: &Self::ReturnedWidget) {
         self.reorder_first(widget);
     }
 }
