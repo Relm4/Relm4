8287a81660c9c2e7dc410c3dfd8bb31ba4d22ef5
Michael Aaron Murphy <mmstick@pm.me>
2022-03-28 18:18:57 +0200

    refactor!: Launch commands from ComponentSender, removing type Command from trait

--- relm4/src/component/sender.rs
+++ relm4/src/component/sender.rs
@@ -1,7 +1,8 @@
 // Copyright 2022 System76 <info@system76.com>
 // SPDX-License-Identifier: MIT or Apache-2.0
 
-use crate::{Component, Sender};
+use crate::{Component, Sender, ShutdownReceiver};
+use std::future::Future;
 use std::sync::Arc;
 
 /// Contain senders used by the component.
@@ -10,14 +11,32 @@ pub type ComponentSender<C> = Arc<ComponentSenderInner<C>>;
 /// Contains senders used by the component.
 #[derive(Debug)]
 pub struct ComponentSenderInner<C: Component> {
+    /// Emits command outputs
+    pub(crate) command: Sender<C::CommandOutput>,
+
     /// Emits component inputs
     pub input: Sender<C::Input>,
 
     /// Emits component outputs
     pub output: Sender<C::Output>,
+
+    pub(crate) shutdown: ShutdownReceiver,
 }
 
 impl<C: Component> ComponentSenderInner<C> {
+    /// Spawn a command managed by the lifetime of the component.
+    pub fn command<Cmd, Fut>(&self, cmd: Cmd)
+    where
+        Cmd: (Fn(Sender<C::CommandOutput>, ShutdownReceiver) -> Fut) + Send + Sync + 'static,
+        Fut: Future<Output = ()> + Send,
+    {
+        let recipient = self.shutdown.clone();
+        let sender = self.command.clone();
+        crate::spawn(async move {
+            cmd(sender, recipient).await;
+        });
+    }
+
     /// Emit an input to the component.
     pub fn input(&self, message: C::Input) {
         self.input.send(message);
