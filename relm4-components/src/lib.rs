909fa59bb1c134853ad8d70581417015bdb6c1c7
Aaron Erhardt <aaron.erhardt@t-online.de>
2022-03-30 19:51:56 +0200

    Fix formatting
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- relm4-components/src/lib.rs
+++ relm4-components/src/lib.rs
@@ -1,9 +1,7 @@
 //! Collection of reusable and easily configurable components for Relm4.
 
 #![warn(missing_docs, rust_2018_idioms, unreachable_pub)]
-#![doc(
-    html_logo_url = "https://raw.githubusercontent.com/Relm4/relm4/main/assets/Relm_logo.svg"
-)]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/Relm4/relm4/main/assets/Relm_logo.svg")]
 #![doc(
     html_favicon_url = "https://raw.githubusercontent.com/Relm4/relm4/main/assets/Relm_logo.svg"
 )]
