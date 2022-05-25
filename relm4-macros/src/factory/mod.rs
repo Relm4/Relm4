3b1542286bd31c1ebbd3ac7518a55ce14d71d5a9
Aaron Erhardt <aaron.erhardt@t-online.de>
2022-05-25 13:36:24 +0200

    Add Debug impl to factory widgets
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- relm4-macros/src/factory/mod.rs
+++ relm4-macros/src/factory/mod.rs
@@ -132,6 +132,7 @@ pub(crate) fn generate_tokens(
     quote! {
         #[allow(dead_code)]
         #outer_attrs
+        #[derive(Debug)]
         #vis struct #widgets_type {
             #struct_fields
             #additional_fields
