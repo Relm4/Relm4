8337ed727502b1b1d1e26eba949e91fd5f77b709
Andy Russell <arussell123@gmail.com>
2022-07-31 11:05:32 -0400

    pass senders by value in to_do example

--- examples/to_do.rs
+++ examples/to_do.rs
@@ -74,7 +74,7 @@ impl FactoryComponent for Task {
     fn init_model(
         name: Self::InitParams,
         _index: &DynamicIndex,
-        _sender: &FactoryComponentSender<Self>,
+        _sender: FactoryComponentSender<Self>,
     ) -> Self {
         Self {
             name,
@@ -131,7 +131,7 @@ impl SimpleComponent for AppModel {
         }
     }
 
-    fn update(&mut self, msg: AppMsg, _sender: &ComponentSender<Self>) {
+    fn update(&mut self, msg: AppMsg, _sender: ComponentSender<Self>) {
         match msg {
             AppMsg::DeleteEntry(index) => {
                 self.tasks.guard().remove(index.current_index());
@@ -142,7 +142,7 @@ impl SimpleComponent for AppModel {
         }
     }
 
-    fn init(_param: (), root: &Self::Root, sender: &ComponentSender<Self>) -> ComponentParts<Self> {
+    fn init(_param: (), root: &Self::Root, sender: ComponentSender<Self>) -> ComponentParts<Self> {
         let widgets = view_output!();
 
         let model = AppModel {
