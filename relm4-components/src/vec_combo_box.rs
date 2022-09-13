dea1da2b4c1a8e01d911286d4a64e8e4f7436500
Aaron Erhardt <aaron.erhardt@t-online.de>
2022-09-13 23:01:28 +0200

    components: Extend VecComboBox
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- relm4-components/src/vec_combo_box.rs
+++ relm4-components/src/vec_combo_box.rs
@@ -1,23 +1,51 @@
-use relm4::gtk;
 use relm4::gtk::prelude::{ComboBoxExt, ComboBoxExtManual};
 use relm4::{component, ComponentParts, SimpleComponent};
+use relm4::{gtk, ComponentSender};
+
+pub struct VecComboBox {
+    data: VecComboBoxData,
+    root: gtk::ComboBoxText,
+}
 
 #[derive(Debug)]
-pub struct VecComboBox;
+pub struct VecComboBoxData {
+    pub strings: Vec<String>,
+    pub active_index: usize,
+}
+
+#[derive(Debug)]
+pub enum VecComboBoxInput {
+    UpdateData(VecComboBoxData),
+    #[doc(hidden)]
+    UpdateIndex(usize),
+}
 
 #[component(pub)]
 impl SimpleComponent for VecComboBox {
-    type Input = ();
+    type Input = VecComboBoxInput;
     type Output = usize;
-    type Init = Vec<String>;
+    type Init = VecComboBoxData;
     type Widgets = EnumComboBoxWidget;
 
     view! {
         gtk::ComboBoxText {
             connect_changed => move |combo_box| {
                 if let Some(active_idx) = combo_box.active() {
-                    sender.output(active_idx as usize);
+                    sender.input(Self::Input::UpdateIndex(active_idx as usize));
                 }
+            }
+        }
+    }
+
+    fn update(&mut self, input: Self::Input, sender: ComponentSender<Self>) {
+        match input {
+            VecComboBoxInput::UpdateIndex(idx) => {
+                sender.output(idx);
+                self.data.active_index = idx;
+            }
+            VecComboBoxInput::UpdateData(data) => {
+                self.data = data;
+                self.render();
             }
         }
     }
@@ -27,14 +55,25 @@ impl SimpleComponent for VecComboBox {
         root: &Self::Root,
         sender: relm4::ComponentSender<Self>,
     ) -> ComponentParts<Self> {
-        let model = Self;
-
         let widgets = view_output!();
 
-        for (idx, string) in init.iter().enumerate() {
-            root.insert_text(idx as i32, string);
-        }
+        let model = Self {
+            data: init,
+            root: root.clone(),
+        };
+
+        model.render();
 
         ComponentParts { model, widgets }
+    }
+}
+
+impl VecComboBox {
+    fn render(&self) {
+        for (idx, string) in self.data.strings.iter().enumerate() {
+            self.root.insert_text(idx as i32, string);
+        }
+
+        self.root.set_active(u32::try_from(self.data.active_index).ok());
     }
 }
