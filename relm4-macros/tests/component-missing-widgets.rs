1b4eb49457c107353c13e65ad392dcc718004401
Aaron Erhardt <aaron.erhardt@t-online.de>
2022-11-05 13:33:02 +0100

    macros: Add test case for missing Widget type
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

---
+++ relm4-macros/tests/component-missing-widgets.rs
@@ -0,0 +1,28 @@
+use relm4::{gtk, ComponentParts, ComponentSender, SimpleComponent};
+
+struct TestComponent;
+
+#[relm4_macros::component]
+impl SimpleComponent for TestComponent {
+    type Init = ();
+    type Input = ();
+    type Output = ();
+
+    view! {
+        gtk::Window {}
+    }
+
+    fn init(
+        _: Self::Init,
+        root: &Self::Root,
+        _sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let model = Self;
+
+        let widgets = view_output!();
+
+        ComponentParts { model, widgets }
+    }
+}
+
+fn main() {}
