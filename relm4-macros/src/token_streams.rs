ca1ce7941c3f7b54d37c4db8da66d0da0566c6d7
Aaron Erhardt <aaron.erhardt@t-online.de>
2023-01-19 22:18:44 +0100

    pre-fix
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- relm4-macros/src/token_streams.rs
+++ relm4-macros/src/token_streams.rs
@@ -144,7 +144,9 @@ impl Widget {
         if generate_init_root_stream {
             if let Some(root_name) = root_name {
                 streams.rename_root.extend(quote_spanned! {
-                    name_span => let #name = #root_name.clone();
+                    name_span => 
+                        #[allow(clippy::redundant_clone)]
+                        let #name = #root_name.clone();
                 });
             }
         }
