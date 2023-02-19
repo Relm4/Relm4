5c7690eded6cd7f46f563b8c9da3e5ef7c0461bd
Aaron Erhardt <aaron.erhardt@t-online.de>
2023-02-19 11:28:50 +0100

    components: Add web image component

---
+++ relm4-components/examples/web_image.rs
@@ -0,0 +1,78 @@
+use gtk::prelude::*;
+use relm4::{
+    gtk, Component, ComponentController, ComponentParts, ComponentSender, Controller, RelmApp,
+    SimpleComponent,
+};
+use relm4_components::web_image::{WebImage, WebImageMsg};
+
+const IMAGES: &[&str] = &[
+    "https://raw.githubusercontent.com/Relm4/Relm4/main/assets/Relm_logo_with_text.png",
+    "https://raw.githubusercontent.com/Relm4/Relm4/main/assets/Relm_logo.png",
+    "https://raw.githubusercontent.com/gtk-rs/gtk-rs.github.io/master/logo/gtk-rs.ico",
+    "https://avatars.githubusercontent.com/u/5430905",
+];
+
+#[derive(Debug)]
+enum AppMsg {
+    Next,
+}
+
+struct App {
+    image: Controller<WebImage>,
+    idx: usize,
+}
+
+#[relm4::component]
+impl SimpleComponent for App {
+    type Init = ();
+    type Input = AppMsg;
+    type Output = ();
+
+    view! {
+        gtk::ApplicationWindow {
+            set_default_size: (300, 300),
+
+            #[wrap(Some)]
+            set_titlebar = &gtk::HeaderBar {
+                pack_start = &gtk::Button {
+                    set_label: "Next image",
+                    connect_clicked => AppMsg::Next,
+                }
+            },
+
+            gtk::Box {
+                #[local_ref]
+                image -> gtk::Box {}
+            }
+        }
+    }
+
+    fn update(&mut self, msg: Self::Input, _: ComponentSender<Self>) {
+        match msg {
+            AppMsg::Next => {
+                self.idx = (self.idx + 1) % IMAGES.len();
+                self.image
+                    .emit(WebImageMsg::LoadImage(IMAGES[self.idx].to_owned()));
+            }
+        }
+    }
+
+    fn init(
+        _: Self::Init,
+        root: &Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let image = WebImage::builder().launch(IMAGES[0].to_owned()).detach();
+        let model = App { image, idx: 0 };
+
+        let image = model.image.widget();
+        let widgets = view_output!();
+
+        ComponentParts { model, widgets }
+    }
+}
+
+fn main() {
+    let app = RelmApp::new("relm4.example.open_button");
+    app.run::<App>(());
+}
---
+++ relm4-components/src/web_image.rs
@@ -0,0 +1,141 @@
+use std::collections::VecDeque;
+use std::fmt::Debug;
+
+use relm4::gtk::prelude::Cast;
+use relm4::gtk::traits::{BoxExt, WidgetExt};
+use relm4::{gtk, ComponentSender};
+use relm4::{Component, ComponentParts};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct WebImage {
+    current_id: usize,
+    current_widget: gtk::Widget,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum WebImageMsg {
+    LoadImage(String),
+}
+
+impl Component for WebImage {
+    type CommandOutput = Option<(usize, VecDeque<u8>)>;
+    type Input = WebImageMsg;
+    type Output = ();
+    type Init = String;
+    type Root = gtk::Box;
+    type Widgets = ();
+
+    fn init_root() -> Self::Root {
+        gtk::Box::default()
+    }
+
+    fn init(
+        url: Self::Init,
+        root: &Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let widget = gtk::Box::default();
+        root.append(&widget);
+        let current_widget = Self::set_spinner(root, widget.upcast_ref());
+
+        let model = Self {
+            current_id: 0,
+            current_widget,
+        };
+
+        model.load_image(&sender, url);
+
+        ComponentParts { model, widgets: () }
+    }
+
+    fn update(
+        &mut self,
+        input: Self::Input,
+        sender: ComponentSender<Self>,
+        root: &Self::Root,
+    ) {
+        match input {
+            WebImageMsg::LoadImage(url) => {
+                self.current_widget = Self::set_spinner(root, &self.current_widget);
+                self.current_id = self.current_id.wrapping_add(1);
+                self.load_image(&sender, url);
+            }
+        }
+    }
+
+    fn update_cmd(
+        &mut self,
+        message: Self::CommandOutput,
+        sender: ComponentSender<Self>,
+        root: &Self::Root,
+    ) {
+        if let Some((id, data)) = message {
+            if id == self.current_id {
+                if let Some(img) = Self::generate_image(data) {
+                    self.current_widget = Self::set_image(root, &self.current_widget, &img);
+                    sender.output(()).ok();
+                }
+            }
+        }
+    }
+}
+
+impl WebImage {
+    #[must_use]
+    fn set_spinner(root: &<Self as Component>::Root, widget: &gtk::Widget) -> gtk::Widget {
+        root.remove(widget);
+        relm4::view! {
+            #[local_ref]
+            root -> gtk::Box {
+                set_halign: gtk::Align::Center,
+                set_valign: gtk::Align::Center,
+
+                #[name(spinner)]
+                gtk::Spinner {
+                    start: (),
+                    set_hexpand: true,
+                    set_vexpand: true,
+                }
+            }
+        }
+        spinner.upcast()
+    }
+
+    #[must_use]
+    fn set_image(
+        root: &<Self as Component>::Root,
+        widget: &gtk::Widget,
+        img: &gtk::Image,
+    ) -> gtk::Widget {
+        root.remove(widget);
+        relm4::view! {
+            #[local_ref]
+            root -> gtk::Box {
+                set_halign: gtk::Align::Fill,
+                set_valign: gtk::Align::Fill,
+
+                #[local_ref]
+                img -> gtk::Image {
+                    set_hexpand: true,
+                    set_vexpand: true,
+                }
+            }
+        }
+        img.clone().upcast()
+    }
+
+    fn load_image(&self, sender: &ComponentSender<Self>, url: String) {
+        sender.oneshot_command(Self::get_img_data(self.current_id, url));
+    }
+
+    fn generate_image(data: VecDeque<u8>) -> Option<gtk::Image> {
+        let pixbuf = gtk::gdk_pixbuf::Pixbuf::from_read(data).ok()?;
+        Some(gtk::Image::from_pixbuf(Some(&pixbuf)))
+    }
+
+    async fn get_img_data(id: usize, url: String) -> Option<(usize, VecDeque<u8>)> {
+        let response = reqwest::get(url).await.ok()?;
+        let bytes = response.bytes().await.ok()?;
+        Some((id, bytes.into_iter().collect()))
+    }
+}
