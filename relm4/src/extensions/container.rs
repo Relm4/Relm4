89606f0c8f56b7bffe0704b5b863754be08cc766
myuujiku <myujiku@proton.me>
2023-10-02 17:36:29 +0900

    implement RelmContainerExt for NavigationView

--- relm4/src/extensions/container.rs
+++ relm4/src/extensions/container.rs
@@ -67,4 +67,12 @@ mod libadwaita {
             self.add(widget.as_ref());
         }
     }
+
+    #[cfg(feature = "gnome_45")]
+    impl RelmContainerExt for adw::NavigationView {
+        fn container_add(&self, widget: &impl AsRef<gtk::Widget>) {
+            use gtk::prelude::Cast;
+            self.add(widget.as_ref().downcast_ref::<adw::NavigationPage>().expect("Not a adw::NavigationPage."));
+        }
+    }
 }
