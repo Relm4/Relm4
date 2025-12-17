2d3437c80bbcd4ea854bfb455bec398c5af3ca91
Zeke Fast <me@zekefast.com>
2025-12-17 20:14:13 +0100

    Add API documentation for relm4::typed_view::selection_ext::RelmSelectionExt trait.
    This is required since it becomes public.

--- relm4/src/typed_view/selection_ext.rs
+++ relm4/src/typed_view/selection_ext.rs
@@ -1,7 +1,27 @@
 use gtk::{gio, prelude::IsA};
 
+/// Allows to create new or change existing [`gtk::SelectionModel`](gtk::SelectionModel)
+/// for Relm's type safe abstractions
+/// over [`gtk::ColumnView`](gtk::ColumnView)
+/// (i.e. [`TypedColumnView`](crate::typed_view::column::TypedColumnView)),
+/// [`gtk::ListView`](gtk::ListView)
+/// (i.e. [`TypedListView`](crate::typed_view::list::TypedListView))
+/// and [`gtk::GridView`](gtk::GridView)
+/// (i.e. [`TypedGridView`](crate::typed_view::grid::TypedGridView)).
 pub trait RelmSelectionExt: IsA<gtk::SelectionModel> {
+    /// Creates new [`gtk::SelectionModel`](gtk::SelectionModel) to view it is implemented on
+    ///
+    /// Typically called during `init`
+    /// phase of [`TypedColumnView`](crate::typed_view::column::TypedColumnView),
+    /// [`TypedListView`](crate::typed_view::list::TypedListView)
+    /// or [`TypedGridView`](crate::typed_view::grid::TypedGridView)
+    /// to provide a chosen selection model for view instantiation.
     fn new_model(model: gio::ListModel) -> Self;
+
+    /// Set provided [`gio::ListModel`](gio::ListModel) on the chosen (i.e. [`Self`]) selection model
+    ///
+    /// Usually it is useful to do during the addition of new filters,
+    /// in that case the existing model has to be replaced with provided [`gtk::FilterListModel`](gtk::FilterListModel).
     fn set_list_model(&mut self, model: &gio::ListModel);
 }
 
