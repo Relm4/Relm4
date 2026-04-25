d4989e576b395530eb303c822c0b57b898ddf461
bahrom04 <magdiyevbahrom@gmail.com>
2026-04-25 18:42:42 +0500

    refactor: move shell.nix content inside flake.nix

--- flake.nix
+++ flake.nix
@@ -6,25 +6,67 @@
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     # Fresh and new for testing
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # The flake-utils library
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-  # @ inputs
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      # Nix script formatter
-      formatter = pkgs.alejandra;
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    # @ inputs
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        # Nix script formatter
+        formatter = pkgs.nixfmt;
 
-      # Development environment
-      devShells.default = import ./shell.nix {inherit pkgs;};
-    });
+        # Development environment
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Nix
+            nixd
+            statix
+            deadnix
+            nixfmt
+
+            # Rust
+            rustc
+            cargo
+            rustfmt
+            clippy
+            rust-analyzer
+            cargo-watch
+            openssl
+            # libressl
+
+            # Gnome related
+            gtk4
+            gtk4
+            meson
+            ninja
+            pango
+            parted
+            polkit
+            gdk-pixbuf
+            libadwaita
+            pkg-config
+            gnome-desktop
+            appstream
+            appstream-glib
+            wrapGAppsHook4
+            desktop-file-utils
+            gobject-introspection
+            rustPlatform.bindgenHook
+          ];
+        };
+      }
+    );
 }
